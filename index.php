c9acf96946c0e3ed908fcee91fea9cdf91062cee
retarf <retarf@gmail.com>
2017-03-31 09:33:49 +0200

    CSS-heart

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="wrap">
+		<div class="screen">
+			<div class="heart">
+				<div class="left-arm"></div>
+				<div class="right-arm"></div>
+				<div class="angle">
+					<div class="squere"></div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</body>
+</html>
